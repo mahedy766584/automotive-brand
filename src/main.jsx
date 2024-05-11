4ada6382925176224da78ea55d48c5e81c23addb
mahedy766584 <108580163+mahedy766584@users.noreply.github.com>
2024-05-11 12:04:57 +0600

    new filr

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import myCreatedRouter from './Router/Router'
 import AuthProvider from './Authentication/AuthProvider/Authprovider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={myCreatedRouter} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={myCreatedRouter} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
